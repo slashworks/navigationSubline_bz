711792a900672f1cecee7e904958b11c950fb17b
Joe Ray Gregory <jrgregory@Joes-Mac-mini-2.local>
2014-09-16 13:47:38 +0200

    Initial commit

---
+++ navigationSubline_bz/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2013 Leo Feyer
+ * 
+ * @package NavigationSubline_bz
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'nav_withSubline' => 'system/modules/navigationSubline_bz/templates',
+));
